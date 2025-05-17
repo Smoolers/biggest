d2b39d64217136b352828f9a5353bbdbdb295322
Martin Hagegård <martin.hagegard@gmail.com>
2025-05-17 15:16:38 +0200

    Added index.html and index.js

---
+++ JS/index.js
@@ -0,0 +1,28 @@
+let numberOne = prompt("Skriv in ett nummer");
+let numberTwo = prompt("Skriv in ett till nummer");
+
+if (numberOne < numberTwo) {
+  console.log(
+    "Ditt första nummer var " +
+      numberOne +
+      ", vilket är mindre än " +
+      numberTwo +
+      " (ditt andra nummer)"
+  );
+} else {
+  console.log(
+    "Ditt första nummer var " +
+      numberOne +
+      ", vilket är större än " +
+      numberTwo +
+      " (ditt andra nummer)"
+  );
+}
+// const number1 = prompt("Skriv ett nummer");
+// const number2 = prompt("Skriv ett till nummer");
+
+// if (number1 > number2) {
+//console.log("number1 är störst");
+// } else {
+//console.log("number2 är störst");
+// }
